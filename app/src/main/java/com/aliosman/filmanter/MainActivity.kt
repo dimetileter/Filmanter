dd57d93180465007130bd88cf74e628b890fa8b2
ali osman <aliosmankaraogul4334@gmail.com>
2024-09-13 20:45:16 +0300

    added introduce1 animations

--- app/src/main/java/com/aliosman/filmanter/MainActivity.kt
+++ app/src/main/java/com/aliosman/filmanter/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.aliosman.filmanter
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -23,5 +25,18 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val motionLayout = binding.main
+        Handler(Looper.getMainLooper()).postDelayed({
+            motionLayout.transitionToEnd()
+        },  1000)
+
+        val coverMotions = binding.constraintLayout
+        binding.buttonNext.setOnClickListener {
+            Handler(Looper.getMainLooper()).postDelayed({
+                coverMotions.transitionToEnd()
+            },  100)
+        }
+
     }
 }
