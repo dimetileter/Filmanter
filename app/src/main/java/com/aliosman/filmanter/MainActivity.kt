b8549b985b5337fd73a8500dfb329724adaceb2b
ali osman <aliosmankaraogul4334@gmail.com>
2024-09-14 22:51:26 +0300

    Animations are ready

--- app/src/main/java/com/aliosman/filmanter/MainActivity.kt
+++ app/src/main/java/com/aliosman/filmanter/MainActivity.kt
@@ -3,6 +3,8 @@ package com.aliosman.filmanter
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.animation.AnimationUtils
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -26,17 +28,46 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        val motionLayout = binding.main
+        // Tanıtım metnini 1000ms gecikmeli başlat
         Handler(Looper.getMainLooper()).postDelayed({
-            motionLayout.transitionToEnd()
-        },  1000)
+            val introText1 = getString(R.string.introduce1)
+            introduceTextAnimation(introText1)
+        }, 1000)
 
-        val coverMotions = binding.constraintLayout
+        // Butona tıklandığında sırasıyla animasyonları getir.
+        var num = 0
         binding.buttonNext.setOnClickListener {
-            Handler(Looper.getMainLooper()).postDelayed({
-                coverMotions.transitionToEnd()
-            },  100)
+            binding.introduce.text = null
+            when(num) {
+                0 -> secondIntroduce()
+                1 -> thirdIntroduce()
+            }
+            num += 1
         }
+    }
 
+    // Giriş metni için animasyon oluştur
+    private fun introduceTextAnimation(msg: String) {
+        val introduceText = binding.introduce
+        introduceText.text = msg
+        val animate = AnimationUtils.loadAnimation(this, R.anim.introduce_text_anim)
+        introduceText.startAnimation(animate)
+    }
+
+    // Butona tıklandığında ikinci
+    private fun secondIntroduce() {
+        val coverMotions = binding.constraintLayout
+        val introText2 = getString(R.string.introduce2)
+        Handler(Looper.getMainLooper()).postDelayed({
+            coverMotions.transitionToEnd()
+        },  100)
+        Handler().postDelayed({
+            introduceTextAnimation(introText2)
+        }, 400)
+    }
+
+    // Butona tıklandığında 3. tanıtım sayfaınsa git
+    private fun thirdIntroduce() {
+        Toast.makeText(this, "[Test]İkinci tıklama", Toast.LENGTH_SHORT).show()
     }
 }
